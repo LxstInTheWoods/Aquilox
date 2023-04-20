c3f4dfb4a0b787084959816ac2e6a97a575b4f97
LxstInTheWoods <soltober7764@icloud.com>
2023-04-20 09:27:51 -0500

    Yes

--- js/slideshow/slideshow.js
+++ js/slideshow/slideshow.js
@@ -7,57 +7,46 @@ const image = document.getElementById("IMGG")
 var current = 0
 
 
-//move in                             images[parseInt(v)].animate([{right:"0%"}], {duration:1500, fill:"forwards"})
+
 
 async function Set(){
+    let p = new Promise( async (resolve) =>{
     for (const [v, x] of Object.entries(images))
     {
 
-        const p = new Promise((Resolve) =>{
-                //moves the image into frame
-                x.style.display = "block"
+        let _pinternal = new Promise((intres)=>{
+  images[parseInt(v)].animate([{right:"0%"}], {duration:1500, fill:"forwards"})
 
-            async function _RR()
-            {   
-                //move the next one into the frame
-                const _p = new Promise((_R) => {
-                        console.log(images.length - 1, parseInt(v))
-                        if (parseInt(v) === images.length - 1)
-                        {
-                            console.log(images[parseInt(v)])
-                            images[parseInt(v)  ].animate([{right:"0%"}], {duration:1500, fill:"forwards"})
-                            console.log(images[parseInt(v)])
+            setTimeout(() => {
+                intres()
+            }, 1500);
 
 
-                        }
-                        else
-                        {
-                            images[parseInt(v) + 1 ].animate([{right:"1000%"}], {duration:1500, fill:"forwards"})
-                            console.log(images[parseInt(v)])
-
-                        }
-                        setTimeout(() => {
-                            _R()
-                        }, 2000);
-
-
-                })
-                
-                //moves the previous out of the frame
-                await _p.then((ONf)=>{  
-
-                           // x.animate([{right:"1000%"}], {duration:1500, fill:"forwards"})
-                            setTimeout(() => {
-                                Resolve()
-                            },1800 );
-
-                 } )
-        }
-        _RR()
         })
-        await p.then((F)=>{})
+
+        await _pinternal.then(()=>{
+            if (parseInt(v) === images.length - 1)
+            {
+                setTimeout(() => {
+                    images[parseInt(v) ].animate([{right:"1000%"}], {duration:250, fill:"forwards"})
+                }, 1500);
+            }
+            else{images[parseInt(v) + 1 ].animate([{right:"1000%"}], {duration:1500, fill:"forwards"})}
+
+        })
+
     }
-    Set()
+
+    resolve()
+
+}) //end promise
+
+    await p.then((r) =>{
+        console.log("Done")
+        Set()
+    })
 }
 
 Set()   
+//in images[parseInt(v)  ].animate([{right:"0%"}], {duration:1500, fill:"forwards"})
+//out images[parseInt(v) + 1 ].animate([{right:"1000%"}], {duration:1500, fill:"forwards"})
